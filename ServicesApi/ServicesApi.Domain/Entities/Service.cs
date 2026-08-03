1afbe8188a203d144b808a0a0aec33f15d8e6cc3
lapivan <lapunoujohn@gmail.com>
2026-08-03 17:17:10 +0300

    Add base models

---
+++ ServicesApi/ServicesApi.Domain/Entities/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace ServicesApi.Domain.Entities;
+
+public class Service
+{
+    public Guid Id { get; init; } =  Guid.NewGuid();
+    public Guid ServiceId { get; set; }
+    public Guid ServiceCategory { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public bool IsActive { get; set; }
+}
