d15a28fb0e5cb04b5adab2244726ec4b53a4dd60
Lapunov Ivan <lapunoujohn@gmail.com>
2026-08-04 11:36:05 +0300

    Add base models (#2)

---
+++ ServicesApi/ServicesApi.Domain/Entities/ServiceCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace ServicesApi.Domain.Entities;
+
+public class ServiceCategory
+{
+    public Guid Id { get; init; } =  Guid.NewGuid();
+    public string Name { get; set; }
+    public TimeSpan Duration { get; set; }
+}
