9e49134c57ec1b9246354f3802befaa6514d919b
Lapunov Ivan <lapunoujohn@gmail.com>
2026-08-06 17:05:23 +0300

    Add mappings (#7)

--- ServicesApi/ServicesApi.API/Program.cs
+++ ServicesApi/ServicesApi.API/Program.cs
@@ -1,3 +1,4 @@
+using ServicesApi.Application;
 using ServicesApi.Infrastructure;
 using ServicesApi.Infrastructure.Persistence;
 
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
---
+++ ServicesApi/ServicesApi.Application/Mappings/ServiceCategoryMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ServicesApi.Application.Dto.ServiceCategories;
+using ServicesApi.Domain.Entities;
+
+namespace ServicesApi.Application.Mappings;
+
+public sealed class ServiceCategoryMappingProfile : Profile
+{
+    public ServiceCategoryMappingProfile()
+    {
+        CreateMap<ServiceCategory, ServiceCategoryDto>();
+        CreateMap<AddServiceCategoryDto, ServiceCategory>();
+        CreateMap<UpdateServiceCategoryDto, ServiceCategory>();
+    }
+}
